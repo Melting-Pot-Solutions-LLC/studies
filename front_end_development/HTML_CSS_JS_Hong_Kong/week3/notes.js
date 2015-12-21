015b3cd35ccfc1c0d1f4e3f92f39f7bc7634fa2b
steveEECSrubin <konstantinrubin@engineer.com>
2015-12-21 21:24:42 +0300

    week3

---
+++ front_end_development/HTML_CSS_JS_Hong_Kong/week3/notes.js
@@ -0,0 +1,49 @@
+<h1>THIS FILE CONTAINS LECTURE NOTES FROM WEEK3</h1>
+
+<h3>dialog window</h3>
+<code>alert("...");
+confirm("...");
+prompt("...");
+</code>
+
+<h3>3 types of varibales: NUMBER, STRING, BOOLEAN double and floats are the same </h3>
+<code>
+var x = 1000;
+typeof(x); 
+var name = "David"
+var bool_true = true;
+</code>
+
+<h3>hange the types of the variables</h3>
+<code>
+ParseInt(...)
+ParseFloat(...)
+String(...)
+</code>
+
+
+<h3>declare a new function</h3>
+<code>
+function do_sth(...)
+{
+	
+}
+</code>
+
+
+<h3>local variables have more priority</h3>
+
+<h3>predefined funcitons <h3>
+<code>onload = "alert("...")"</code>
+
+<h3>What the arrays look like:<h3>
+<code>
+var pets = ["Dogs", "Cat", "Rabbit"];
+pets.join(' and'); //"Dogs and Cat and Rabbit"
+pets.push("hamster"); //append at the end
+pets.unshift("house"); //append at the beginning
+pets.shift(); //remote from the front
+var numbers = [2, 10, 15]
+pets.concat(number);  //return the concated arrays
+Math.floor(); //take the flooring of the number
+</code>
