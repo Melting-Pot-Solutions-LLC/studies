140f6fcd9fd66a055736ebb896a926acb4f652a8
Steve Rubin <KonstantinRubin@engineer.com>
2016-09-12 13:56:36 -0400

    Update from_hackerrankcom.py

--- coding_supporting_routines/python/from_hackerrankcom.py
+++ coding_supporting_routines/python/from_hackerrankcom.py
@@ -20,3 +20,24 @@
 # very
 # long
 # string.
+------------------------------------------------------------------------------------------------------------------
+
+#.ljust(width)
+#This method returns a left aligned string of length width.
+>>> width = 20
+>>> print 'HackerRank'.ljust(width,'-')
+#HackerRank----------  
+
+#.center(width)
+#This method returns a centered string of length width.
+>>> width = 20
+>>> print 'HackerRank'.center(width,'-')
+#-----HackerRank-----
+
+#.rjust(width)
+#This method returns a right aligned string of length width.
+>>> width = 20
+>>> print 'HackerRank'.rjust(width,'-')
+#----------HackerRank
+
+------------------------------------------------------------------------------------------------------------------
