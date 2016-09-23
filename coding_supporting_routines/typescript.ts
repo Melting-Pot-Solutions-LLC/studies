389bc580ea8c4738e2942665fc5b29043f3c2df3
Steve Rubin <KonstantinRubin@engineer.com>
2016-09-23 12:31:11 -0400

    Update typescript.ts

--- coding_supporting_routines/typescript.ts
+++ coding_supporting_routines/typescript.ts
@@ -4,3 +4,7 @@ getRandomInteger(min, max)
   console.log("getRandomInteger");
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+//convert a number to a string
+new_time = "click me";
+this.new_time = ""+(this.getRandomInteger(1000, 4000));
